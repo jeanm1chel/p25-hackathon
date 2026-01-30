f92d68a36b32f84d7ad0fdce63eb8a30f987c0bd
Gabriel Chaleil <gabriel.chaleil@etu.minesparis.psl.eu>
2026-01-30 10:43:41 +0100

    un peu d'herbe

---
+++ grass.py
@@ -0,0 +1,42 @@
+import math
+import random as randint
+
+class Grass:
+    def __init__(self, Grille, prob_pousse, temps_repousse) :
+        self.Grille = Grille
+        self.herbe = []
+        self.herbe_mangé = {}
+    
+    def initialisation(self) :
+        C = 0
+        nb_grass = math.floor(0.3*(len(self.grille))**2)
+        while C < nb_grass :
+            x = rd.randint(0, len(self.grille))
+            y = rd.randint(0, len(self.grille))
+            if (x,y) in self.coord :
+                C += 0
+            else :
+                self.herbe.append((x,y))
+                C += 1
+                self.Grille.grille[x,y] = W
+        
+    def nouvelle_herbe(self) :
+        for i in range(n) :
+            for j in range(n) :
+                if (i,j) in self.herbe :
+                    0 = 0
+                else :
+                    m = rd.uniform()
+                        if m < prob_repousse :
+                            self.append((i,j))
+                            self.Grille.grille[i,j] = G
+
+    def repousse_herbe(self) : #le faire tout à la fin car réinitialise direct pour le prochain
+        for c in self.herbe_mangé :
+            self.herbe_mangé[c] += 1
+            if self.herbe_mangé = temps_repousse :
+                self.herbe.append(c)
+                self.Grille.grille[c[0], c[1]] = G
+
+        
+    
