74b187260bfa009b708d9b1f5156ef3607e6b1d7
jeanm1chel <epitre1.m2@gmail.com>
2026-01-30 11:34:12 +0100

    commun 2

--- commun.py
+++ commun.py
@@ -6,3 +6,9 @@ from animals import Animals
 from grass import Grass
 from grille import Grille
 
+n = 10
+prob_repousse = 0.05
+temps_repousse = 7
+
+grille = Grille(n)
+grass = Grass(grille, prob_repousse, temps_repousse)
--- grille.py
+++ grille.py
@@ -7,7 +7,6 @@ class Grille:
         for ligne in self:
             for case in ligne:
                 case[0] = animals()
-                case[0].type = "."
                 case[1] = "."
     
     def voisin(self, pos):
